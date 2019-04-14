7f256b87301417bcc1e02759fd6c78c544326211
AugustoSavi <guto_savi@outlook.com>
2019-04-14 12:36:05 -0300

    Ainda em Desenvolvimento

---
+++ Classe_hash.h
@@ -0,0 +1,101 @@
+#include "pessoa.h"
+#include <list>
+#include<fstream>
+
+#define TAM 5
+
+class Classe_hash{
+   private:
+      list<pessoa> plist[TAM];
+
+   public:
+      Classe_hash();
+      ~Classe_hash();
+      void insere(pessoa p);
+      void remove(int cod);
+      void consulta(int cod);
+      void consultanome(string nome);
+      void imprimirhash();
+      int calc_hash(int cod);
+};
+
+Classe_hash::Classe_hash(){}
+
+ Classe_hash::~Classe_hash()
+ {
+ 	for(int i=0;i<TAM;i++)
+ 	{
+ 		plist[i].clear();
+	 }
+}
+
+void Classe_hash::insere(pessoa p)
+{
+	int num=calc_hash(p.getcodigo());
+	plist[num].push_back(p);
+
+}
+
+void Classe_hash::remove(int cod)
+{
+	int num=calc_hash(cod);
+	list<pessoa> *h = &plist[num];
+	list<pessoa>::iterator it;
+
+	 for(it = h->begin(); it != h->end(); it++){
+        if(it->getcodigo() == cod){
+           h->erase(it);
+           break;
+        }
+    }
+}
+
+void Classe_hash::consulta(int cod)
+{
+	int num=calc_hash(cod);
+	list<pessoa> h = plist[num];
+	list<pessoa>::iterator it;
+
+	 for(it = h.begin(); it != h.end(); it++){
+        if(it->getcodigo() == cod){
+           it->imprimir();
+           break;
+        }
+    }
+
+}
+
+void Classe_hash::consultanome(string nome)
+{
+
+	list<pessoa> h ;
+	list<pessoa>::iterator it;
+
+	 for(int classe = 0; classe < TAM; classe++){
+        h = plist[classe];
+        for(it = h.begin(); it != h.end(); it++){
+            if(it->getnome() == nome){
+                 it->imprimir();
+                 break;
+            }
+        }
+    }
+
+}
+
+int Classe_hash::calc_hash(int cod)
+{
+	return cod % 5;
+}
+
+void Classe_hash::imprimirhash()
+{
+	list<pessoa> h;
+	list<pessoa>:: iterator it;
+	for(int classe = 0; classe < TAM; classe++){
+        h = plist[classe];
+        for(it = h.begin(); it != h.end(); it++){
+            it->imprimir();
+
+}
+}}
---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <cstdlib>
+#include <iostream>
+#include "Classe_hash.h"
+#include <fstream>
+
+using namespace std;
+
+
+void menu()
+{
+    cout << "Implementação do Hash" << endl;
+    cout<<"1 - Reiniciar"<<endl;
+    cout<<"2 - Inserir"<<endl;
+    cout<<"3 - Remover"<<endl;
+    cout<<"4 - Consultar Específico - pelo côdigo"<<endl;
+    cout<<"5 - Consultar Específico - pelo nome"<<endl;
+    cout<<"6 - Listar"<<endl;
+    cout<<"0 - Sair"<<endl<<endl;
+}
+
+
+main()
+{
+
+	int op;
+	pessoa p;
+    int codigo;
+    string nome;
+    Classe_hash *tabela = new Classe_hash();
+	do
+	{
+
+	menu();
+	cout << "Informe a opcao: ";
+    cin >> op;
+
+	switch(op)
+	{
+		case 0: exit(0);break;
+		case 1:
+    {
+				delete tabela;
+				tabela=new Classe_hash();
+        fstream arq ("dados_pessoa.txt",ios :: trunc);
+        remove("dados_pessoa.txt");
+        arq.close();
+
+
+    }
+    		break;
+
+		case 2:
+    {
+				cout<<"Informe o seu codigo: "; cin>>codigo;
+				cout<<"Informe o seu nome: "; cin>>nome;
+				p.setcodigo(codigo);
+				p.setnome(nome);
+				tabela->insere(p);
+
+        fstream arq;
+
+        arq.open("dados_pessoa.txt",ios::app | ios :: out );
+        arq<<p.getcodigo()<<"\t"<<p.getnome()<<"\n";
+
+        arq.close();
+    }
+        break;
+
+		case 3:
+    {
+      	cout<<"Informe o seu codigo: "; cin>>codigo;
+				tabela->remove(codigo);
+
+        fstream arq ("dados_pessoa.txt",ios::in|ios :: out);
+
+        if(arq.fail() == 1){
+          cout << "O arquivo a ser lido nao esta gravado em disco" << endl;
+        }
+
+        else{
+              while (!arq.eof())
+              {
+
+
+
+              }
+            }
+    }
+				break;
+
+		case 4:
+				cout<<"Informe o seu codigo: "; cin>>codigo;
+				tabela->consulta(codigo);
+				break;
+
+		case 5:
+				cout<<"Informe o seu nome: "; cin>>nome;
+				tabela->consultanome(nome);
+				break;
+
+		case 6:
+				tabela->imprimirhash();
+				break;
+
+
+
+	}
+    //system("Pause");
+    getchar();
+    cout<<"\n\n\n\n";
+	}while(op!=0);
+}
